c6e2db0ef9f0e3c595de28a43dc573f6fcf215c9
Munyvann <kimhak.prome@gmail.com>
2026-07-13 16:42:56 +0700

    feat:create admin login page and navbar

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -101,7 +101,7 @@ const removeProduct = async (req, res) => {
 
 const singleProduct = async (req, res) => {
   try {
-    const { productId } = req.budy;
+    const { productId } = req.body;
     const product = await productModel.findById(productId);
     res.json({ success: true, product });
   } catch (error) {
