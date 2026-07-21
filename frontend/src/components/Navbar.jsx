6ed0a1b4499e42558520ed7cb9e8e35035f277db
Munyvann <kimhak.prome@gmail.com>
2026-07-21 10:56:22 +0700

    Align Favorites navbar icon badge sizing and positioning with Cart icon

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -68,7 +68,7 @@ export default function Navbar() {
           )}
         </div>
 
-        <Link to="/favorites" className="relative text-[#5A3A31] hover:opacity-80 transition-opacity">
+        <Link to="/favorites" className="relative flex items-center">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -83,11 +83,9 @@ export default function Navbar() {
               d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
             />
           </svg>
-          {getFavoritesCount() > 0 && (
-            <p className="absolute -right-1.5 bottom-1.25 aspect-square w-4 rounded-full bg-[#5A3A31] text-center text-[8px] leading-4 text-white font-bold">
-              {getFavoritesCount()}
-            </p>
-          )}
+          <p className="absolute -right-1.5 bottom-1.25 aspect-square w-4 rounded-full bg-[#5A3A31] text-center text-[8px] leading-4 text-white">
+            {getFavoritesCount()}
+          </p>
         </Link>
 
         <Link to="/cart" className="relative">
